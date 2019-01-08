192c9d0f4feb0941d67482f949749b1ffad16ace
petar <piterjov@gmail.com>
2019-01-08 11:38:35 +0100

    adding mustache

--- public/js/index.js
+++ public/js/index.js
@@ -9,23 +9,32 @@ socket.on('disconnect', function () {
 socket.on('newMessage', function (message) {
     var formattedTime = moment(message.createdAt).format('h:mm: a');
 
-    console.log('You have new message: ', message);
-    var li = jQuery('<li></li>');
-    li.text(`${message.from} ${formattedTime}: ${message.text}`);
-    jQuery('#messages').append(li);
+    var template = jQuery('#message-template').html();
+    var html = Mustache.render(template, {
+        text: message.text,
+        from: message.from,
+        createdAt: formattedTime
+    });
+
+    jQuery('#messages').append(html)
+
+
+
 
 });
 
 socket.on('newLocationMessage', function (message) {
     var formattedTime = moment(message.createdAt).format('h:mm: a');
+    var template = jQuery('#location-message-template').html();
+    var html = Mustache.render(template, {
+        from: message.from,
+        url: message.url,
+        createdAt: formattedTime
+    });
 
-    var li = jQuery('<li></li>');
-    var a = jQuery('<a target="_blank" href=""> My current location </a>');
+    jQuery('#messages').append(html)
 
-    li.text(`${message.from} ${formattedTime}`);
-    a.attr('href', message.url);
-    li.append(a);
-    jQuery('#messages').append(li);
+
 
 });
 
